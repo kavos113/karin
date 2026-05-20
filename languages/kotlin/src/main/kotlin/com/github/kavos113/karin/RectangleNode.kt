00a42302e3f3c4b5d409feea793b4cc0e33089ad
kavos113 <kohei.hadano@gmail.com>
2026-05-20 14:55:55 +0900

    feat: rectangle node native handle

--- languages/kotlin/src/main/kotlin/com/github/kavos113/karin/RectangleNode.kt
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/RectangleNode.kt
@@ -1,9 +1,9 @@
 package com.github.kavos113.karin
 
-import com.github.kavos113.karin.engine.jni.JniRectangleNodeBridge
+import com.github.kavos113.karin.engine.handle.RectangleNodeHandle
 
-class RectangleNode internal constructor(ptr: Long) : ContainerNode(ptr) {
+class RectangleNode(size: Size, color: Color) : ContainerNode(
+    RectangleNodeHandle(size, color)
+) {
 
-    constructor(size: Size, color: Color)
-        : this(JniRectangleNodeBridge.create(size.width, size.height, color.r, color.g, color.b, color.a))
 }
---
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/ui/LayoutDirection.kt
@@ -0,0 +1,6 @@
+package com.github.kavos113.karin.ui
+
+enum class LayoutDirection(val value: Int) {
+    Row(0),
+    Column(1)
+}
