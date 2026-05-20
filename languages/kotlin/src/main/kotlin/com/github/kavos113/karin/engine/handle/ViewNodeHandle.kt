ef3ff2dad4cac6ed5da95e003c7642480047dfbb
kavos113 <kohei.hadano@gmail.com>
2026-05-20 13:54:03 +0900

    feat: viewnode native handle

---
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/engine/handle/ViewNodeHandle.kt
@@ -0,0 +1,33 @@
+package com.github.kavos113.karin.engine.handle
+
+import com.github.kavos113.karin.engine.jni.JniViewNodeBridge
+import com.github.kavos113.karin.engine.memory.NativeResourceManager
+import java.lang.ref.Cleaner
+
+internal open class ViewNodeHandle(ptr: Long) {
+    private var internalPtr = ptr
+    private val cleanable: Cleaner.Cleanable = NativeResourceManager.cleaner.register(this, CleanupTask(ptr))
+
+    val ptr: Long
+        get() {
+            check(internalPtr != 0L) {
+                "ownership of this handle already transferred to C++ side (or destroyed), cannot access it anymore"
+            }
+            return internalPtr
+        }
+
+    fun transferOwnership(): Long {
+        val currentPtr = ptr
+        internalPtr = 0L
+        cleanable.clean()
+        return currentPtr
+    }
+
+    private class CleanupTask(private val ptr: Long) : Runnable {
+        override fun run() {
+            if (ptr != 0L) {
+                JniViewNodeBridge.destroy(ptr)
+            }
+        }
+    }
+}
