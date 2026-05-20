6a3596d4cbb9a2fe07feca4e175852909d4edc3d
kavos113 <kohei.hadano@gmail.com>
2026-05-20 14:58:00 +0900

    feat: text node native handle

--- languages/kotlin/src/main/kotlin/com/github/kavos113/karin/TextNode.kt
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/TextNode.kt
@@ -1,35 +1,14 @@
 package com.github.kavos113.karin
 
-import com.github.kavos113.karin.engine.jni.JniTextNodeBridge
+import com.github.kavos113.karin.engine.handle.TextNodeHandle
 
-class TextNode internal constructor(ptr: Long) : ViewNode(ptr) {
+class TextNode(
+    text: String = "",
+    style: TextStyle = TextStyle(),
+    paragraphStyle: ParagraphStyle = ParagraphStyle(),
+    color: Color = Color.Black
+) : ViewNode(
+    TextNodeHandle(text, style, paragraphStyle, color)
+) {
 
-    constructor(
-        text: String = "",
-        style: TextStyle = TextStyle(),
-        paragraphStyle: ParagraphStyle = ParagraphStyle(),
-        color: Color = Color.Black
-    ) : this(
-        JniTextNodeBridge.create(
-            text,
-            style.fontFamily,
-            style.fontStyle.toInt(),
-            style.fontStretch.toInt(),
-            style.fontWeight.toInt(),
-            style.fontSize,
-            style.locale,
-            false, // TODO
-            false,
-            paragraphStyle.horizontalAlignment.toInt(),
-            paragraphStyle.verticalAlignment.toInt(),
-            paragraphStyle.lineSpacingValue,
-            paragraphStyle.baseLineOffset,
-            paragraphStyle.lineSpacingMode.toInt(),
-            paragraphStyle.trimMode.toInt(),
-            paragraphStyle.wrapMode.toInt(),
-            paragraphStyle.flowDirection.toInt(),
-            paragraphStyle.readingDirection.toInt(),
-            color.r, color.g, color.b, color.a
-        )
-    )
 }
