71625adff68a5f7ed813ce6a0fadf695c8d7ba24
kavos113 <kohei.hadano@gmail.com>
2026-05-21 17:26:22 +0900

    fix: split text style

---
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/ui/text/FontStretch.kt
@@ -0,0 +1,13 @@
+package com.github.kavos113.karin.ui.text
+
+enum class FontStretch(val value: Int) {
+    UltraCondensed(0),
+    ExtraCondensed(1),
+    Condensed(2),
+    SemiCondensed(3),
+    Normal(4),
+    SemiExpanded(5),
+    Expanded(6),
+    ExtraExpanded(7),
+    UltraExpanded(8)
+}
---
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/ui/text/FontStyle.kt
@@ -0,0 +1,7 @@
+package com.github.kavos113.karin.ui.text
+
+enum class FontStyle(val value: Int) {
+    Normal(0),
+    Italic(1),
+    Oblique(2)
+}
