b6296613cd7597c6fa3ce22033c2ca6ab76a061e
kavos113 <kohei.hadano@gmail.com>
2026-05-23 15:11:38 +0900

    feat: state props

--- languages/kotlin/src/main/kotlin/com/github/kavos113/karin/ui/props/Style.kt
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/ui/props/Style.kt
@@ -1,19 +1,27 @@
 package com.github.kavos113.karin.ui.props
 
+import com.github.kavos113.karin.runtime.State
 import com.github.kavos113.karin.ui.common.Color
 
 @ConsistentCopyVisibility
 data class Style private constructor(
     val backgroundColor: Color? = null,
+    val backgroundColorState: State<Color>? = null,
     val opacity: Float? = null,
+    val opacityState: State<Float>? = null
 ) {
     fun background(color: Color) = copy(backgroundColor = color)
+    fun background(state: State<Color>) = copy(backgroundColorState = state)
+
     fun alpha(value: Float) = copy(opacity = value)
+    fun alpha(state: State<Float>) = copy(opacityState = state)
 
     companion object {
         val Default = Style()
 
         fun background(color: Color) = Style().background(color)
+        fun background(state: State<Color>) = Style().background(state)
         fun alpha(value: Float) = Style().alpha(value)
+        fun alpha(state: State<Float>) = Style().alpha(state)
     }
 }
