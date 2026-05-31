19e1be5d1392a29b5c60f3762aecd74b230ddf6c
kavos113 <kohei.hadano@gmail.com>
2026-06-01 08:31:24 +0900

    feat: offscreen renderer interface

--- core/src/graphics/offscreen_renderer.cpp
+++ core/src/graphics/offscreen_renderer.cpp
@@ -1,13 +1,14 @@
 #include <karin/graphics/offscreen_renderer.h>
 
+#include "offscreen_renderer_impl.h"
+
 karin::OffscreenRenderer::OffscreenRenderer(uint32_t width, uint32_t height)
 {
 }
 
-karin::OffscreenRenderer::~OffscreenRenderer()
-{
-}
+karin::OffscreenRenderer::~OffscreenRenderer() = default;
 
 std::vector<std::byte> karin::OffscreenRenderer::getImageData() const
 {
+    return m_offscreenImpl->getImageData();
 }
