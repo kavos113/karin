43fd309c05140225963fbd00fe994444657ac30a
kavos113 <kohei.hadano@gmail.com>
2026-06-05 15:53:59 +0900

    fix script

--- core/scripts/update-graphics-test-fixture-ubuntu.sh
+++ core/scripts/update-graphics-test-fixture-ubuntu.sh
@@ -3,7 +3,7 @@ export TEST_PROFILE="ubuntu"
 
 echo "Updating graphics test fixture for profile: $TEST_PROFILE"
 
-TEST_PATH="$(dirname "$(realpath "$0")")../../build/ubuntu-local-debug/core/test"
+TEST_PATH="$(dirname "$(realpath "$0")")/../../build/ubuntu-local-debug/core/test"
 DEST_PATH="$(dirname "$(realpath "$0")")/../test/fixtures/test_expect_images"
 echo "Test path: $TEST_PATH"
 echo "Destination path: $DEST_PATH"
