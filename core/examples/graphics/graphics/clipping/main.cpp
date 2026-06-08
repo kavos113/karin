bca5444c8d456037c2f9b24842d395bb5cb9b535
kavos113 <kohei.hadano@gmail.com>
2026-06-08 09:20:49 +0900

    feat: add clipping examplei

---
+++ core/examples/graphics/graphics/clipping/main.cpp
@@ -0,0 +1,41 @@
+#include <karin/system.h>
+#include <karin/graphics.h>
+#include <karin/common.h>
+
+#include <memory>
+
+int main()
+{
+    karin::Application& app = karin::Application::instance();
+    std::unique_ptr<karin::Window> window = app.createWindow("Hello Graphics", 100, 100, 800, 600);
+
+    karin::WindowRenderer renderer(window.get());
+
+    karin::Pattern redPattern = karin::SolidColorPattern(karin::Color(karin::Color::Red));
+    karin::Pattern bluePattern = karin::SolidColorPattern(karin::Color(karin::Color::Blue));
+
+    renderer.addDrawCommand(
+        [&redPattern, &bluePattern](karin::GraphicsContext& gc)
+        {
+            gc.withSave([&]
+            {
+                gc.clip(karin::Rectangle(150, 150, 300, 300));
+
+                gc.fillRect(karin::Rectangle(100, 100, 200, 200), redPattern);
+                gc.fillEllipse(karin::Point(400, 300), 100, 50, bluePattern);
+                gc.fillRoundedRect(karin::Rectangle(500, 100, 200, 200), 20, 50, redPattern);
+            });
+        }
+    );
+
+    renderer.update();
+
+    window->setStatus(karin::Window::ShowStatus::SHOW);
+
+    karin::EventPayload event;
+    while (app.waitEvent(event)) {}
+
+    renderer.cleanUp();
+
+    return 0;
+}
