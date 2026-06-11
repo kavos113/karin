7c918794bb8455b9ba25fc588dd9c5f5b45bcd6c
kavos113 <kohei.hadano@gmail.com>
2026-06-12 08:13:38 +0900

    fix for path length

--- core/scripts/update-graphics-test-fixture-windows.ps1
+++ core/scripts/update-graphics-test-fixture-windows.ps1
@@ -3,24 +3,24 @@ $env:TEST_PROFILE="windows"
 
 Write-Host "Updating graphics test fixture for Windows"
 
-Push-Location -Path (Join-Path $PSScriptRoot "..\..\build\windows-local-debug\core\test")
+Push-Location -Path (Join-Path $PSScriptRoot "..\..\build\win-d\core\test")
 
 ./karin_test.exe
 Write-Host "Done updating graphics test fixture for Windows"
 Pop-Location
 
-Copy-Item -Path (Join-Path $PSScriptRoot "..\..\build\windows-local-debug\core\test\fixtures\test_expect_images\windows") -Destination (Join-Path $PSScriptRoot "..\test\fixtures\test_expect_images") -Recurse -Force
+Copy-Item -Path (Join-Path $PSScriptRoot "..\..\build\win-d\core\test\fixtures\test_expect_images\windows") -Destination (Join-Path $PSScriptRoot "..\test\fixtures\test_expect_images") -Recurse -Force
 
 
 $env:TEST_PROFILE="windows-vulkan"
 
 Write-Host "Updating graphics test fixture for Windows Vulkan"
 
-Push-Location -Path (Join-Path $PSScriptRoot "..\..\build\windows-vulkan-local-debug\core\test")
+Push-Location -Path (Join-Path $PSScriptRoot "..\..\build\wvk-d\core\test")
 
 ./karin_test.exe
 
 Write-Host "Done updating graphics test fixture for Windows Vulkan"
 Pop-Location
 
-Copy-Item -Path (Join-Path $PSScriptRoot "..\..\build\windows-vulkan-local-debug\core\test\fixtures\test_expect_images\windows-vulkan") -Destination (Join-Path $PSScriptRoot "..\test\fixtures\test_expect_images") -Recurse -Force
+Copy-Item -Path (Join-Path $PSScriptRoot "..\..\build\wvk-d\core\test\fixtures\test_expect_images\windows-vulkan") -Destination (Join-Path $PSScriptRoot "..\test\fixtures\test_expect_images") -Recurse -Force
