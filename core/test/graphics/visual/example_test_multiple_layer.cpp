8ea41d3d319b96467ff90952428f03ddba9b89b4
kavos113 <kohei.hadano@gmail.com>
2026-07-11 10:38:16 +0900

    feat(core): add multiple layer example test

---
+++ core/test/graphics/visual/example_test_multiple_layer.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+
+#include <numbers>
+
+#include <karin/graphics.h>
+#include <karin/common.h>
+
+#include "visual_tester.h"
+
+TEST(ExamplesVisualTest, MultipleLayer)
+{
+    karin::OffscreenRenderer renderer(800, 600);
+
+    renderer.setClearColor(karin::Color(karin::Color::Green));
+
+    karin::Pattern cyanPattern = karin::SolidColorPattern(karin::Color(0, 255, 255));
+    karin::Pattern magentaPattern = karin::SolidColorPattern(karin::Color(255, 0, 255));
+    karin::Pattern whitePattern = karin::SolidColorPattern(karin::Color(255, 255, 255));
+
+    renderer.addDrawCommand(
+        [&cyanPattern, &magentaPattern, &whitePattern](karin::GraphicsContext& gc)
+        {
+            gc.fillRect(karin::Rectangle(550, 100, 200, 200), whitePattern);
+            gc.fillRect(karin::Rectangle(575, 175, 150, 50), cyanPattern);
+            gc.fillEllipse(karin::Point(650, 200), 50, 50, magentaPattern);
+
+            gc.withLayer(karin::Rectangle(50, 50, 500, 300), 0.7f,
+                [&gc, &whitePattern, &cyanPattern, &magentaPattern]
+                {
+                    gc.withLayer(karin::Rectangle(50, 100, 300, 300), 0.7f,
+                        [&gc, &whitePattern, &cyanPattern, &magentaPattern]
+                        {
+                            gc.fillRect(karin::Rectangle(50, 100, 200, 200), whitePattern);
+                            gc.fillRect(karin::Rectangle(75, 175, 150, 50), cyanPattern);
+                            gc.fillEllipse(karin::Point(150, 200), 50, 50, magentaPattern);
+                        }
+                    );
+
+                    gc.withLayer(karin::Rectangle(300, 100, 300, 300), 0.5f,
+                        [&gc, &whitePattern, &cyanPattern, &magentaPattern]
+                        {
+                            gc.fillRect(karin::Rectangle(300, 100, 200, 200), whitePattern);
+                            gc.fillRect(karin::Rectangle(325, 175, 150, 50), cyanPattern);
+                            gc.fillEllipse(karin::Point(400, 200), 50, 50, magentaPattern);
+                        }
+                    );
+                }
+            );
+        }
+    );
+
+    renderer.draw();
+
+    bool result = VisualTester::checkOrUpdate(
+        "multiple_layer",
+        renderer.getImageData(),
+        800,
+        600
+    );
+    ASSERT_TRUE(result);
+
+    renderer.cleanUp();
+}
