ee3f016b4183b08a3416c629b06534a80f2bf4d7
kavos113 <kohei.hadano@gmail.com>
2026-07-14 14:40:38 +0900

    feat(shaderc): print variable reflection

--- core/tools/slang_shader_compiler/main.cpp
+++ core/tools/slang_shader_compiler/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <print>
 #include <string>
+#include <stdexcept>
 
 #include <slang/slang.h>
 #include <slang/slang-com-ptr.h>
 #include <slang/slang-com-helper.h>
 
+#include "print.h"
+
 void usage(char *program)
 {
     std::println("Usage: {} <.slang file>", program);
+}
+
+void diagnose(slang::IBlob *diagnostics)
+{
+    if (diagnostics != nullptr)
+    {
+        std::println("{}", static_cast<const char *>(diagnostics->getBufferPointer()));
+    }
 }
 
 int main(int argc, char **argv)
@@ -35,7 +46,23 @@ int main(int argc, char **argv)
     Slang::ComPtr<slang::ISession> session;
     globalSession->createSession(sessionDesc, session.writeRef());
 
-    
+    Slang::ComPtr<slang::IBlob> diagnostics;
+    Slang::ComPtr<slang::IModule> module;
+    module = session->loadModule(fileName.c_str(), diagnostics.writeRef());
+    if (!module)
+    {
+        throw std::runtime_error("failed to load module");
+    }
+
+    for (auto decl : module->getModuleReflection()->getChildren())
+    {
+        slang::VariableReflection *varDecl = decl->asVariable();
+
+        if (varDecl && varDecl->findModifier(slang::Modifier::Const) && varDecl->findModifier(slang::Modifier::Static))
+        {
+            printVariable(varDecl);
+        }
+    }
 
     return 0;
 }
---
+++ core/tools/slang_shader_compiler/print.h
@@ -0,0 +1,8 @@
+#ifndef CORE_TOOLS_SLANG_SHADER_COMPILER_PRINT_H
+#define CORE_TOOLS_SLANG_SHADER_COMPILER_PRINT_H
+
+#include <slang/slang.h>
+
+void printVariable(slang::VariableReflection *var);
+
+#endif //CORE_TOOLS_SLANG_SHADER_COMPILER_PRINT_H
