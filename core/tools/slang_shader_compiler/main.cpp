7020a0b0007255415b7e56b1ab67af8458f4c214
kavos113 <kohei.hadano@gmail.com>
2026-07-14 15:11:16 +0900

    feat(shaderc): compile and link program

--- core/tools/slang_shader_compiler/main.cpp
+++ core/tools/slang_shader_compiler/main.cpp
@@ -2,10 +2,12 @@
 #include <print>
 #include <string>
 #include <stdexcept>
+#include <vector>
+#include <ranges>
 
-#include <slang/slang.h>
-#include <slang/slang-com-ptr.h>
-#include <slang/slang-com-helper.h>
+#include <slang.h>
+#include <slang-com-ptr.h>
+#include <slang-com-helper.h>
 
 #include "print.h"
 
@@ -49,20 +51,60 @@ int main(int argc, char **argv)
     Slang::ComPtr<slang::IBlob> diagnostics;
     Slang::ComPtr<slang::IModule> module;
     module = session->loadModule(fileName.c_str(), diagnostics.writeRef());
+    diagnose(diagnostics);
     if (!module)
     {
         throw std::runtime_error("failed to load module");
     }
 
-    for (auto decl : module->getModuleReflection()->getChildren())
-    {
-        slang::VariableReflection *varDecl = decl->asVariable();
+    std::vector<Slang::ComPtr<slang::IComponentType>> componentsToLink;
 
-        if (varDecl && varDecl->findModifier(slang::Modifier::Const) && varDecl->findModifier(slang::Modifier::Static))
+    int definedEntryPoint = module->getDefinedEntryPointCount();
+    std::println("defined entry point: {}", definedEntryPoint);
+    for (int i = 0; i < definedEntryPoint; i++)
+    {
+        Slang::ComPtr<slang::IEntryPoint> entry;
+        SlangResult r = module->getDefinedEntryPoint(i, entry.writeRef());
+        if (SLANG_FAILED(r))
         {
-            printVariable(varDecl);
+            throw std::runtime_error("failed to get entry point");
         }
+
+        std::println("[EntryPoint] {}", entry->getFunctionReflection()->getName());
+
+        componentsToLink.emplace_back(entry.get());
     }
+
+    Slang::ComPtr<slang::IComponentType> composedProgram;
+    SlangResult r = session->createCompositeComponentType(
+        reinterpret_cast<slang::IComponentType**>(componentsToLink.data()),
+        componentsToLink.size(),
+        composedProgram.writeRef(),
+        diagnostics.writeRef()
+    );
+    diagnose(diagnostics);
+    if (SLANG_FAILED(r))
+    {
+        throw std::runtime_error("failed to create composite component type");
+    }
+
+    Slang::ComPtr<slang::IComponentType> program;
+    r = composedProgram->link(program.writeRef(), diagnostics.writeRef());
+    diagnose(diagnostics);
+    if (SLANG_FAILED(r))
+    {
+        throw std::runtime_error("failed to link program");
+    }
+
+    Slang::ComPtr<slang::IBlob> spirvCode;
+    r = program->getEntryPointCode(0, 0, spirvCode.writeRef(), diagnostics.writeRef());
+    diagnose(diagnostics);
+    if (SLANG_FAILED(r))
+    {
+        throw std::runtime_error("failed to get spirv code");
+    }
+
+    std::println("compiled size: {} bytes", spirvCode->getBufferSize());
 
     return 0;
 }
