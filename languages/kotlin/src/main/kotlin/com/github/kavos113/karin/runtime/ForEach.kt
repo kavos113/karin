0eb9924e83e27b6bf4310b6a0966bda959af2f83
kavos113 <kohei.hadano@gmail.com>
2026-08-01 14:24:32 +0900

    feat(kotlin): for each cache

--- languages/kotlin/src/main/kotlin/com/github/kavos113/karin/runtime/ForEach.kt
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/runtime/ForEach.kt
@@ -1,6 +1,7 @@
 package com.github.kavos113.karin.runtime
 
-import com.github.kavos113.karin.engine.handle.ContainerNodeHandle
+import com.github.kavos113.karin.engine.handle.ViewNodeHandle
+import com.github.kavos113.karin.engine.handle.VirtualContainerNode
 import com.github.kavos113.karin.ui.UiBuilder
 
 fun <T, K> UiBuilder.ForEach(
@@ -8,7 +9,20 @@ fun <T, K> UiBuilder.ForEach(
     keySelector: (T) -> K,
     block: UiBuilder.(T) -> Unit
 ) {
+    val components = mutableMapOf<K, List<ViewNodeHandle>>()
+
     for (item in items.value) {
-        block(item)
+        val key = keySelector(item)
+
+        val virtualBuilder = object : UiBuilder() {
+            override val parentContainer = VirtualContainerNode()
+        }
+        virtualBuilder.block(item)
+
+        components[key] = virtualBuilder.parentContainer.children
+    }
+
+    for ((_, v) in components) {
+        v.forEach { parentContainer.addChild(it) }
     }
 }
