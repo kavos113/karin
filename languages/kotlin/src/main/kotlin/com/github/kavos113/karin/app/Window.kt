a06f7a6d51e49d994399589031dd122768f8439d
kavos113 <kohei.hadano@gmail.com>
2026-08-09 15:47:19 +0900

    feat(kotlin): add window dependent and shortcut key handler

--- languages/kotlin/src/main/kotlin/com/github/kavos113/karin/app/Window.kt
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/app/Window.kt
@@ -3,6 +3,8 @@ package com.github.kavos113.karin.app
 import com.github.kavos113.karin.engine.handle.ContainerNodeHandle
 import com.github.kavos113.karin.engine.handle.WindowHandle
 import com.github.kavos113.karin.ui.UiBuilder
+import com.github.kavos113.karin.ui.event.Key
+import com.github.kavos113.karin.ui.event.KeyEvent
 
 class Window(
     title: String,
@@ -14,6 +16,19 @@ class Window(
     private val handle: WindowHandle = WindowHandle(title, x, y, width, height)
     private val rootNode: ContainerNodeHandle = ContainerNodeHandle()
 
+    private val keyHandlers: MutableList<(KeyEvent) -> Unit> = mutableListOf()
+    private val shortcutHandlers: MutableMap<Key, () -> Unit> = mutableMapOf()
+
+    private val keyHandler: (KeyEvent) -> Unit = { e ->
+        for (h in keyHandlers) {
+            h(e)
+        }
+
+        if (shortcutHandlers.containsKey(e.key)) {
+            shortcutHandlers[e.key]!!()
+        }
+    }
+
     fun setContent(
         content: UiBuilder.() -> Unit
     ) {
@@ -24,5 +39,17 @@ class Window(
         builder.content()
 
         handle.setRootView(rootNode)
+    }
+
+    fun addKeyHandler(handler: (KeyEvent) -> Unit) {
+        keyHandlers.add(handler)
+
+        rootNode.setKeyDownHandler(keyHandler)
+    }
+
+    fun addShortcut(key: Key, handler: () -> Unit) {
+        shortcutHandlers[key] = handler
+
+        rootNode.setKeyDownHandler(keyHandler)
     }
 }
