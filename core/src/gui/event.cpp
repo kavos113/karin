7ec45b242fdef7e0f942766c4f4ba204f491fcab
kavos113 <kohei.hadano@gmail.com>
2026-08-19 16:23:18 +0900

    fix(core): timeout lifetime

--- core/src/gui/event.cpp
+++ core/src/gui/event.cpp
@@ -8,19 +8,23 @@ namespace karin::gui
 {
 void setTimeout(Application* target, uint32_t milliseconds, const std::function<void()>& handler)
 {
-
     uint32_t id = target->addActionEventHandler([handler](const std::any& data)
     {
         handler();
     });
 
-    std::mutex mtx;
-    std::condition_variable_any cv;
-
-    std::jthread th([milliseconds, target, id, &mtx, &cv](const std::stop_token& s)
+    struct TimeoutState
     {
-        std::unique_lock lock(mtx);
-        cv.wait_for(lock, s, std::chrono::milliseconds(milliseconds), []{ return false; });
+        std::mutex mtx;
+        std::condition_variable_any cv;
+        std::jthread th;
+    };
+    auto state = std::make_shared<TimeoutState>();
+
+    state->th = std::jthread([milliseconds, target, id, state](const std::stop_token& s)
+    {
+        std::unique_lock lock(state->mtx);
+        state->cv.wait_for(lock, s, std::chrono::milliseconds(milliseconds), []{ return false; });
 
         if (s.stop_requested())
         {
@@ -30,9 +34,9 @@ void setTimeout(Application* target, uint32_t milliseconds, const std::function<
         target->sendActionEvent(id, std::any());
     });
 
-    target->registerDisposable([&th]
+    target->registerDisposable([state]
     {
-        th.request_stop();
+        state->th.request_stop();
     });
 }
 }
